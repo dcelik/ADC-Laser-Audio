25a93577fcb9f091de6e530b7926ac20dd35d732
Jacob Riedel <jr112394@gmail.com>
2015-11-30 12:25:00 -0500

    ADD: First Arduino write up

---
+++ SerialLaserIn/SerialLaserIn.ino
@@ -0,0 +1,27 @@
+int incomingByte = 0;
+int laser = 13;
+int j;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(laser,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    // say what you got:
+    Serial.print("I wrote: ");
+    Serial.println(incomingByte, DEC);
+    int i;
+    for (i = 0; i < 8; i++) {
+      j = bitRead(incomingByte,i);
+      digitalWrite(laser,j);
+      Serial.println(j);
+      //incomingByte >>= 1;  // shift the bits in lotsofbits right by one bit (equivalent to dividing by 2)
+    }
+  }
+}
