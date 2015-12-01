2c1dd0a2de7fd4800b7ceafbcac365cf240022c8
Jacob Riedel <jr112394@gmail.com>
2015-12-01 10:09:03 -0500

    EDIT: Renamed for clarity

---
+++ Recv/Recv.ino
@@ -0,0 +1,33 @@
+int reciever = 8;
+int val;
+int t;
+float TotF = 0;
+int Ones = 0;
+float Freq;
+int next;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(reciever,INPUT);
+  next = millis();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//  t = millis();
+//  val = digitalRead(reciever);
+//  if (val == 0){
+//    Ones++;
+//  }
+//  TotF++;
+//  if (t == (next+1000)){
+//    Freq = TotF/Ones*1.0;
+//    TotF = 0;
+//    Ones = 0;
+//    Serial.println(Freq);
+//    next = millis();
+//  }
+  Serial.print("I recieved: ");
+  Serial.println(abs(val-1));
+}
