2358aba645f262ff850373e3c841c335813f037b
Jacob Riedel <jacob.riedel@students.olin.edu>
2015-12-08 11:49:01 -0500

    UPDATE: Recv now uses its own loop to minimize main loop issue

--- Recv/Recv.ino
+++ Recv/Recv.ino
@@ -16,10 +16,14 @@
 #define DEBUG;               // Turns on print statements that are useful for debugging
                              // Comment out if you want least verbose code
 
+byte intro = B00000000;
+unsigned long data = 0;
+byte ending = B00000000;
+
 byte wrd = 00000000;          // byte to store our currently sampling byte
 unsigned long lastmillis = 0; // last time recorded (ms)
 unsigned long curmillis = 0;  // current time recorded (ms)
-const int interval = 5;       // interval between sampling in milliseconds
+const int interval = 25;       // interval between sampling in milliseconds
 int wrdsamples = 0;           // number of bits sampled in our byte
 
 void setup() {
@@ -31,26 +35,90 @@ void setup() {
 }
 
 void loop() {
-  curmillis = millis();
-  if(curmillis - lastmillis >=interval){ // check if it has been interval milliseconds since we sampled
-    lastmillis = curmillis ;
-    int readval = PINB & 00000001;       // read values of all pins from 8-13 and only take Pin 8
-    if(readval!=0){                      // check if pin is 1 or 0;
-      if(wrdsamples==8){
-        #ifdef DEBUG
-          Serial.println(wrdsamples,BIN);// for debugging
-        #endif
-        wrdsamples = 0;
+
+  int readval = PINB & 00000001;
+  if(readval!=0){ // check if pin is 1 or 0;
+    //intro = readval;
+    for(int index = 47; index >=0; index--){
+      curmillis = millis();
+      if(curmillis - lastmillis > interval){
+        lastmillis = curmillis;
+        int readval = PINB & B00000001;
+        if(index>39){
+          intro = (intro<<1) + readval;
+        }
+        else if(index>7){
+          data = (data << 1) + readval;
+        }
+        else{
+          ending = (ending << 1) + readval;
+        }
       }
-      else{                      // This assumes that the bytes are sent from most to least significant bit.
-        wrd += (PINB & 00000001);// grab value of pin 8
-        wrd<<=1;                 
-        wrdsamples+1;            // shift value over by 1
+      else{
+        index ++;
+        //Serial.print(curmillis - lastmillis >= interval);
       }
     }
-    else{
-      wrd<<=1;
-    }
+  #ifdef DEBUG
+  Serial.println();
+    printbin(intro);
+    printdata();
+    printbin(ending);
+  #endif
+  //delay(interval);
   }
+  
+//    curmillis = millis();
+//    if(curmillis - lastmillis >=interval){ // check if it has been interval milliseconds since we sampled
+//      lastmillis = curmillis ;
+//      int readval = PINB & 00000001;       // read values of all pins from 8-13 and only take Pin 8
+//      if (intro != 0 && wrdsamples < 8){
+//        intro = (intro<<1) + readval;
+//        wrdsamples++;
+//      }
+//      else if (intro != 0 && wrdsamples < 40){
+//        data = (data << 1) + readval;
+//        wrdsamples++;
+//      }
+//      else if (intro != 0 && wrdsamples > 39 ){
+//        ending = (ending << 1) + readval;
+//        wrdsamples++;
+//      }
+//      else if(readval!=0 && intro == 0){ // check if pin is 1 or 0;
+//        intro = readval;
+//        wrdsamples++;
+//      }
+//    } 
+//    if(wrdsamples == 47){
+//      #ifdef DEBUG
+//        Serial.println(intro,BIN);// for debugging
+//        Serial.println(data,BIN);
+//        Serial.println(ending,BIN);
+//      #endif
+//      wrdsamples = 0;
+//      intro = 0;
+//      data = 0;
+//      ending = 0;
+//   }
 }
+
+void printbin(byte b){
+  Serial.print((b>>7) & 1);
+  Serial.print((b>>6) & 1);
+  Serial.print((b>>5) & 1);
+  Serial.print((b>>4) & 1);
+  Serial.print((b>>3) & 1);
+  Serial.print((b>>2) & 1);
+  Serial.print((b>>1) & 1);
+  Serial.print((b>>0) & 1);
+  Serial.println();
+}
+
+void printdata(){
+  for(int k = 31; k>=0; k--){
+    Serial.print((data>>k) & 1);
+  }
+  Serial.println();
+}
+
 
