f41c6d414fe0f28a8947f559b7a117a6855db44b
Jacob Riedel <jacob.riedel@students.olin.edu>
2015-12-13 21:47:22 -0500

    EDIT: Working with .wav in folder

--- RasPiSender.py
+++ RasPiSender.py
@@ -3,21 +3,27 @@
 import wave
 
 def stereo_to_mono(hex1, hex2):
-    """average two hex string samples"""
-    return hex((ord(hex1) + ord(hex2))/2)
+	"""average two hex string samples"""
+	return hex((ord(hex1) + ord(hex2))/2)
 
-wr = wave.open('piano2.wav','r')
+wr = wave.open('flesh_wound.wav','r')
 
 nchannels, sampwidth, framerate, nframes, comptype, compname =  wr.getparams()
 
-ww = wave.open('piano_mono.wav','wb')
+ww = wave.open('flesh_wound_mono.wav','wb')
 ww.setparams((1,sampwidth,framerate,nframes,comptype,compname))
 
 frames = wr.readframes(wr.getnframes()-1)
 
+print ww
+
 new_frames = ''
 
 for (s1, s2) in zip(frames[0::2],frames[1::2]):
-    new_frames += stereo_to_mono(s1,s2)[2:].zfill(2).decode('hex')
+	print ' '.join('{0:08b}'.format(ord(x), 'b') for x in s1)
+	print ' '.join('{0:08b}'.format(ord(x), 'b') for x in s2)
+	print ''
+	new_frames += stereo_to_mono(s1,s2)[2:].zfill(2).decode('hex')
 
-ww.writeframes(bytes(new_frames))
+
+ww.writeframes((new_frames))
