b7b77eae7d82787b4f21ca7ff62a1e28ef9c2ca3
Deniz Celik <deniz.celik@students.olin.edu>
2015-12-17 10:12:50 -0500

    Added c file

--- RasPiSender.py
+++ RasPiSender.py
@@ -1,11 +1,11 @@
-import RPi.GPIO as IO
+#import RPi.GPIO as IO
 import wave
-import cinpy
+#import cinpy
 
 pin = 27
 
-IO.setmode(IO.BCM)
-IO.setup(pin, IO.OUT)
+#IO.setmode(IO.BCM)
+#IO.setup(pin, IO.OUT)
 
 
 wr = wave.open('flesh_wound.wav','rb')
@@ -22,14 +22,17 @@ byteList = []
 for s1 in list(frames):
 	byteList.append(' '.join('{0:08b}'.format(ord(x), 'b') for x in s1))
 	# print byte
-for byte in byteList:
-	for i in byte:
-	##Set code to access GPIO here on RasPi. Will print Byte from MSB.
-		if i == '1':
-			IO.output(pin, IO.HIGH)
-		elif i == '0':
-			IO.output(pin, IO.LOW)
 
-IO.output(pin, IO.LOW)
+print len(byteList);
 
-IO.cleanup()
+#for byte in byteList:
+#	for i in byte:
+#	##Set code to access GPIO here on RasPi. Will print Byte from MSB.
+#		if i == '1':
+#			IO.output(pin, IO.HIGH)
+#		elif i == '0':
+#			IO.output(pin, IO.LOW)
+
+#IO.output(pin, IO.LOW)
+
+#IO.cleanup()
---
+++ sender.c
@@ -0,0 +1,12 @@
+int main ( int argc, char *argv)
+{
+	if(argc != 1){
+		printf("WRONG");
+	}
+	else
+	{
+		printf("%s", argv[0]);
+		printf("%s", argv[1]);
+	}
+
+}
