3c86eb6053407be5fc1329cdaf1488658e1d0903
Jacob Riedel <jacob.riedel@students.olin.edu>
2015-12-18 08:15:39 -0500

    bitchoice code

--- Decoder.py
+++ Decoder.py
@@ -42,7 +42,7 @@ import wave
 wr = wave.open('flesh_wound.wav','rb')
 
 nchannels, sampwidth, framerate, nframes, comptype, compname =  wr.getparams()
-
+temp = 0
 j = '0'
 count = 0
 new_frames = ''
@@ -50,30 +50,52 @@ bit = ''
 f = open( 'temp.txt', 'r' )
 string = f.read()
 f.close()
-
+byte = '' 
+offset = 7
 for i in string:
-	
-	if (i == j):
-		count += 1
-
-	else:
-		if (count <= 10 and count > 3):
-			bit += j
-			count = 0
-		else:
-			loop = count/6
-			for i in range(int(loop)):
-				bit += j
-				count = 0
-		j = str(1-int(j))
-		count += 1
-	
-	if len(bit) == 8:
+	byte += i
+	count += 1
+	if count == 52:
+		x = 0
+		while(x < 52):
+			y = x+offset
+			b = byte[x:y]
+			x+=7
+			for z in b:
+				temp+=int(z)
+			if temp>=3:
+				bit+='1'
+				temp = 0
+			elif temp<3:
+				bit+='0'
+				temp = 0
 		new_frames += (chr(int(bit,2)))
 		bit = ''
-	elif len(bit) > 8:
-		new_frames += (chr(int(bit[0:8],2)))
-		bit = bit[8:]
+		temp = 0
+		count = 0
+		byte = ''
+
+	# if (i == j):
+	# 	count += 1
+
+	# else:
+	# 	if (count>=4 and count<10):
+	# 		bit += j
+	# 		count = 0
+	# 	elif (count >= 100):
+	# 		loop = count/6.5
+	# 	elif(count >= 42):
+	# 		loop = count/6.2
+	# 	else:
+	# 		loop = count/5.9
+	# 	for i in range(int(loop)):
+	# 		bit += j
+	# 		count = 0
+	# 	j = str(1-int(j))
+	# 	count += 1
+
+	
+	
 # print string
 # print list(new_frames)
 
@@ -87,7 +109,6 @@ for i in range(len(string)):
 		j+=8
 f.close()
 
-print len(old_frames)
 
 if len(old_frames)>len(new_frames):
 	leng = len(new_frames)
@@ -97,18 +118,21 @@ else:
 	leng = len(old_frames)
 
 fail = False
-for i in range(leng): 
-	if old_frames[i] != new_frames[i]:
-		print 'FAILED TEST at '+str(i)
-		print ord(old_frames[i])
-		print ord(new_frames[i])
-		print ''
-		fail=True
-if (fail):
-	print 'Recieved and sent bitstreams are different. Check above for error location.'
-else:
-	print 'ALL BITS EQUAL'
-	print 'Be happy cause it worked...'
+for i in range(leng):
+ 	print ''.join('{0:08b}'.format(ord(old_frames[i]), 'b'))
+	print ''.join('{0:08b}'.format(ord(new_frames[i]), 'b'))
+	print ''
+	# if old_frames[i] != new_frames[i]:
+	# 	print 'FAILED TEST at '+str(i)
+	# 	print ''.join('{0:08b}'.format(ord(old_frames[i]), 'b'))
+	# 	print ''.join('{0:08b}'.format(ord(new_frames[i]), 'b'))
+	# 	print ''
+	# 	fail=True
+# if (fail):
+# 	print 'Recieved and sent bitstreams are different. Check above for error location.'
+# else:
+# 	print 'ALL BITS EQUAL'
+# 	print 'Be happy cause it worked...'
 
 print len(old_frames)
 print len(new_frames)
